e0cc6c2a34335ce1eca4a57f7c8d5c71b0640d58
Bhargava Borra <bhargava.borra@gmail.com>
2019-09-16 15:10:50 +0530

    Logic change for 2 way DataBinding

--- src/app/name/name-display.component.ts
+++ src/app/name/name-display.component.ts
@@ -9,12 +9,23 @@ export class NameDisplayComponent implements OnInit {
 
   constructor() { }
   public name: string;
+  public i: number; 
   public Displaylowercase:string ="";
   public Displayuppercase: string ="";
 
   public caseDivide(){
-    this.Displaylowercase = this.name.toLowerCase();
-    this.Displayuppercase = this.name.toUpperCase();
+    this.Displaylowercase="";
+    this.Displayuppercase="";
+    for(this.i=0;this.i<this.name.length;this.i++){
+      if(isNaN(<number><any>this.name[this.i])){
+        if(this.name[this.i]==this.name[this.i].toLowerCase()){
+          this.Displaylowercase=this.Displaylowercase+this.name[this.i];
+        }
+        else if(this.name[this.i]==this.name[this.i].toUpperCase()){
+          this.Displayuppercase=this.Displayuppercase+this.name[this.i];
+        }
+      } 
+    }
   }
 
   ngOnInit() {
